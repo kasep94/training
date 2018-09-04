e7db2d5af05df1d03de76746acc2db7e9dac7756
nodeJ <994037715@qq.com>
2018-09-04 14:43:05 +0800

    添加课程编辑页面

--- src/components/edit-card/data.js
+++ src/components/edit-card/data.js
@@ -2,19 +2,19 @@ const data = [
   {
     info: '阅读 30分钟/天',
     name: 'read',
-    icon: '../../../static/images/all/course_read.png',
+    icon: 'https://okcdn.apiweixin.top/asserts/img/ic4.jpg',
     id: '10001'
   },
   {
     info: '做家务 3次/周',
     name: 'housework',
-    icon: '../../../static/images/all/course_housework.png',
+    icon: 'https://okcdn.apiweixin.top/asserts/img/ic3.jpg',
     id: '10002'
   },
   {
     info: '户外运动 60分钟/天',
     name: 'run',
-    icon: '../../../static/images/all/course_run.png',
+    icon: 'https://okcdn.apiweixin.top/asserts/img/ic5.jpg',
     id: '10003'
   }
 ]
---
+++ src/pages/course-add/index.vue
@@ -0,0 +1,63 @@
+/**
+ * 文档作者: kasepa94
+ * 创建时间：2018年09月04日
+ * 修改时间：2018年09月04日
+ * 描述信息：编辑课程页面
+ */
+<template>
+    <div class="course-add-page flex-content-center">
+        <div class="content">
+            <div class="label-inp">
+                <p><i class="icon icon-book" />课程名称</p>
+                <input class="btn-1" type="text" placeholder="填写课程名称" />
+            </div>
+            <div class="label-inp">
+                <p><i class="icon icon-mechanism" />机构名称</p>
+                <input class="btn-1" type="text" placeholder="填写机构名称" />
+            </div>
+            <div class="label-inp">
+                <p><i class="icon icon-time" />上课时间</p>
+                <div class="time-select">
+                    <input class="btn-1" type="text" placeholder="选择课程时间"/>
+                    <i class="icon icon-right"/>
+                </div>
+            </div>
+            <div class="label-inp">
+                <p><i class="icon icon-address" />上课地点</p>
+                <div class="time-select">
+                    <input class="btn-1" type="text" placeholder="选择课程时间"/>
+                    <i class="icon icon-right"/>
+                </div>
+            </div>
+            <p class="btn-1 add-course">添加课程</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  computed: {},
+  methods: {}
+};
+</script>
+
+<style scoped lang='less'>
+.course-add-page {
+  .content {
+    width: 690rpx;
+  }
+  .label-inp {
+    margin-top: 40rpx;
+  }
+  .icon-address {
+    height: 35rpx;
+  }
+  .add-course {
+    background: @cl-4;
+    color: white;
+    margin: 0 auto;
+    width: 542rpx;
+    margin-top: 188rpx;
+  }
+}
+</style>
