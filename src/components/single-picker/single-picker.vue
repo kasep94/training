9e483b74ebce510567287ba4c5e770c689383be4
nodeJ <994037715@qq.com>
2018-09-25 21:23:51 +0800

    添加课程

---
+++ src/components/single-picker/single-picker.vue
@@ -0,0 +1,45 @@
+/**
+ * 文档作者: kasepa94
+ * 创建时间：2018年09月25日
+ * 修改时间：2018年09月25日
+ * 描述信息：单选
+ */
+
+<template>
+  <view class="section">
+    <picker @change="bindTimePickerChange" :value="timeIndex" :range="data" range-key="name">
+      <view class="picker">
+        <div class="time-select">
+            <input disabled class="btn-1" type="text" :placeholder="info"/>
+        </div>
+      </view>
+    </picker>
+  </view>
+</template>
+
+<script>
+
+export default {
+  props: {
+    data: {
+      type: Array
+    },
+    info: {
+      type: String
+    }
+  },
+  data() {
+    return {
+      timeIndex: 0
+    };
+  },
+  methods: {
+    bindTimePickerChange: function(e) {
+      console.log(e.mp.detail.value)
+      this.$emit("onPicker", e.mp.detail.value);
+    }
+  }
+};
+</script>
+
+
--- src/pages/edit-course/index.vue
+++ src/pages/edit-course/index.vue
@@ -7,15 +7,18 @@
 <template>
     <div class="edit-course-page flex-content-center">
         <div class="content">
-            <div class="label-inp">
-                <p><i class="icon icon-book" />课程名称</p>
-                <input class="btn-1" type="text" v-model="data.name" placeholder="填写书籍名称" />
-            </div>
 
             <div class="label-inp">
                 <p><i class="icon icon-book" />机构名称</p>
-                <input class="btn-1" type="text" v-model="data.merchantName" placeholder="填写书籍名称" />
+                <SinglePicker :data='mechanismPicker' @onPicker='onMechanismPicker' :info="saveMPicker && saveMPicker.name || '请选择机构'" />
             </div>
+            
+            <div class="label-inp">
+                <p><i class="icon icon-book" />课程名称</p>
+                <input v-if="!saveMPicker" @click="onInfo" class="btn-1" placeholder='请选择课程名称'/>
+                <SinglePicker v-else :data='coursePicker' @onPicker='onCoursePicker' :info="saveCPicker.name || '请选择课程名称'" />
+            </div>
+            
             <div class="label-inp">
                 <p><i class="icon icon-time" />上课时间</p>
                 <div class="time-select" v-for="(item, i) of timeArr" :key="i">
@@ -24,10 +27,10 @@
                 </div>
                 <Picker @onTimePicker='onSelectTime' info='选择课程时间' type='time' />
             </div>
-            <div class="label-inp">
+            <!-- <div class="label-inp">
                 <p><i class="icon icon-book" />上课地点</p>
                 <input class="btn-1" type="text" v-model="data.addr" placeholder="填写书籍名称" />
-            </div>
+            </div> -->
 
             <p @click="onSubmit" class="btn-1 add-course">保  存</p>
         </div>
@@ -37,8 +40,10 @@
 <script>
 import Picker from "../../components/picker/picker";
 import service from "../course/service.js";
+import SinglePicker from "../../components/single-picker/single-picker";
 
 export default {
+  components: { Picker, SinglePicker },
   data() {
     return {
       // 选中的时间
@@ -47,22 +52,31 @@ export default {
       apiArr: [],
       // course页面带过来到参数
       data: {},
-      // 判断是修改还是添加
-      hasEdit: false
+      // {'add' | 'edit'}是修改还是添加
+      type: "",
+      // 机构picker数据
+      mechanismPicker: [],
+      // 保存选择的机构Picker数据
+      saveMPicker: null,
+      // 课程picker数据
+      coursePicker: [],
+      // 保存选择的机构Picker数据
+      saveCPicker: {}
     };
   },
   onLoad(option) {
     this.data = service.getData();
-    this.hasEdit = option.hasOwnProperty("type");
-    console.log(this.data)
+    this.type = option.type;
     // 获取所有机构
     global.PUBLIC.util.httpGet(`/merchants/search?query=`, {}).then(res => {
-      console.log(res.data);
+      this.mechanismPicker = res.data.map(v => {
+        return {
+          ...v,
+          name: v.org_name
+        };
+      });
     });
-    // 获取所有课程
-    global.PUBLIC.util.httpGet(`/merchants/class?query=`, {}).then(res => {
-      console.log(res.data);
-    });
+
     if (option.hasOwnProperty("hasData")) {
       if (option.hasData === "add") {
         // TODO
@@ -87,12 +101,46 @@ export default {
       this.timeArr = [];
     }
   },
-  components: { Picker },
   mounted() {
     global.PUBLIC.util.setTitle("编辑课程");
   },
   computed: {},
   methods: {
+    /** 提示 */
+    onInfo() {
+      wx.showToast({
+        title: "请先选择机构",
+        icon: "none",
+        duration: 2000
+      });
+    },
+    /** 选择机构
+     * @param {Object} index 数组索引
+     * @memberof single-picker
+     */
+    onMechanismPicker(index) {
+      this.saveMPicker = this.mechanismPicker[index];
+      // 获取所有课程
+      global.PUBLIC.util
+        .httpGet(`/merchant/class`, {
+          dp_code: this.saveMPicker.dp_code
+        })
+        .then(res => {
+          this.coursePicker = res.data.map(v => {
+            return {
+              ...v,
+              name: v.item_name
+            };
+          });
+        });
+    },
+    /** 选择课程
+     * @param {Object} index 数组索引
+     * @memberof single-picker
+     */
+    onCoursePicker(index) {
+      this.saveCPicker = this.coursePicker[index] || {};
+    },
     /** 删除选中的日期
      * @param {Array} node 节点属性
      * @param {number} index 索引
@@ -108,8 +156,8 @@ export default {
         if (v === node) {
           this.apiArr.splice(i, 1);
         }
-        return v === node
-      })
+        return v === node;
+      });
     },
     /** 获取日期选择的数据
      * @param {Array} data 选择的数据
@@ -121,32 +169,42 @@ export default {
     },
     /** 保存 */
     onSubmit() {
-      if (this.apiArr.length > 0) {
-        global.PUBLIC.util
-          .httpOther("POST", `/rule/batch`, {
-            batch: global.PUBLIC.util.conversionDate(this.apiArr).map(v => {
-              return {
-                ...v,
-                type: "leaning",
-                object_id: this.data.id || this.data.schedule.id,
-                trainee_id: this.data.trainee_id
-              };
-            })
-          })
-          .then(res => {
-            if (res.code !== 200) {
-              wx.showToast({
-                title: res.message,
-                icon: "none",
-                duration: 2000
+      global.PUBLIC.util
+        .httpOther("POST", `/lesson/user`, {
+          trainee_id: global.PUBLIC.util.getUser().trainee_id,
+          class_id: this.saveCPicker.id
+        })
+        .then(res => {
+          return res.data;
+        })
+        .then(res => {
+          if (this.apiArr.length > 0) {
+            global.PUBLIC.util
+              .httpOther("POST", `/rule/batch`, {
+                batch: global.PUBLIC.util.conversionDate(this.apiArr).map(v => {
+                  return {
+                    ...v,
+                    type: "lesson",
+                    object_id: res.id,
+                    trainee_id: global.PUBLIC.util.getUser().trainee_id
+                  };
+                })
+              })
+              .then(res => {
+                if (res.code !== 0) {
+                  wx.showToast({
+                    title: res.message,
+                    icon: "none",
+                    duration: 2000
+                  });
+                } else {
+                  wx.navigateBack({ changed: true });
+                }
               });
-            } else {
-              wx.navigateBack({ changed: true });
-            }
-          });
-      } else {
-        wx.navigateBack({ changed: true });
-      }
+          } else {
+            wx.navigateBack({ changed: true });
+          }
+        });
     }
   }
 };
